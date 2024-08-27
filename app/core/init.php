8ff18bacd3f6f82558057555bf03843f9bc1361e
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-27 21:40:44 +0100

    files required in the init.php file

--- app/core/init.php
+++ app/core/init.php
@@ -10,8 +10,11 @@ spl_autoload_register(function($classname){
 });
 
 require 'config.php';
-require 'functions.php';
-require 'Database.php';
-require 'Model.php';
 require 'Controller.php';
+require 'Database.php';
+require 'functions.php';
+require 'Model.php';
+require 'Pager.php';
+require 'Request.php';
+require 'Session.php';
 require 'App.php';
