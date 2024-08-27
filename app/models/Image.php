c63c80d0ce95031bbac8717ac804cf7d56c4dfcf
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-08-27 21:38:36 +0100

    adjsted

--- app/models/Image.php
+++ app/models/Image.php
@@ -37,69 +37,68 @@ class Image{
 				default:
 					return $filename;
 					break;
+		}
+
+		$src_w = imagesx($image);
+		$src_h = imagesy($image);
+
+		if($src_w > $src_h){
+			//reduce max size if image is smaller
+			if($src_w < $max_size)
+			{
+			$max_size = $src_w;
 			}
 
-			$src_w = imagesx($image);
-			$src_h = imagesy($image);
+			$dst_w = $max_size;
+			$dst_h = ($src_h / $src_w) * $max_size;
+		}else{
 
-		    if($src_w > $src_h){
-		      //reduce max size if image is smaller
-		      if($src_w < $max_size)
-		      {
-		        $max_size = $src_w;
-		      }
-
-		      $dst_w = $max_size;
-		      $dst_h = ($src_h / $src_w) * $max_size;
-		    }else{
-
-		      //reduce max size if image is smaller
-		      if($src_h < $max_size)
-		      {
-		        $max_size = $src_h;
-		      }
-
-		      $dst_w = ($src_w / $src_h) * $max_size;
-		      $dst_h = $max_size;
-		    }
-
-		    $dst_w = round($dst_w); 
-		    $dst_h = round($dst_h);
-		    
-			$dst_image = imagecreatetruecolor($dst_w, $dst_h);
-
-			if($type == 'image/png'){
-
-				imagealphablending($dst_image, false);
-				imagesavealpha($dst_image, true );
+			//reduce max size if image is smaller
+			if($src_h < $max_size)
+			{
+			$max_size = $src_h;
 			}
 
-			imagecopyresampled($dst_image, $image, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
+			$dst_w = ($src_w / $src_h) * $max_size;
+			$dst_h = $max_size;
+		}
 
-			imagedestroy($image);
+		$dst_w = round($dst_w); 
+		$dst_h = round($dst_h);
+		
+		$dst_image = imagecreatetruecolor($dst_w, $dst_h);
 
-			switch ($type) {
+		if($type == 'image/png'){
 
-				case 'image/png':
-					imagepng($dst_image,$filename,8);
-					break;
+			imagealphablending($dst_image, false);
+			imagesavealpha($dst_image, true );
+		}
 
-				case 'image/gif':
-					imagegif($dst_image,$filename);
-					break;
-				
-				case 'image/jpeg':
-					imagejpeg($dst_image,$filename,90);
-					break;
-				
-				case 'image/webp':
-					imagewebp($dst_image,$filename,90);
-					break;
-				
-				default:
-					imagejpeg($dst_image,$filename,90);
-					break;
-			}
+		imagecopyresampled($dst_image, $image, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
+		imagedestroy($image);
+
+		switch ($type) {
+
+			case 'image/png':
+				imagepng($dst_image,$filename,8);
+				break;
+
+			case 'image/gif':
+				imagegif($dst_image,$filename);
+				break;
+			
+			case 'image/jpeg':
+				imagejpeg($dst_image,$filename,90);
+				break;
+			
+			case 'image/webp':
+				imagewebp($dst_image,$filename,90);
+				break;
+			
+			default:
+				imagejpeg($dst_image,$filename,90);
+				break;
+		}
 
 			imagedestroy($dst_image);
 		}
