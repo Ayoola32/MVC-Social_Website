50e6294806e4e7514a044a33deb826aecca16ddf
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-02 11:17:54 +0100

    define the path for the core and model files and include the file if exists

--- app/core/init.php
+++ app/core/init.php
@@ -6,15 +6,36 @@ spl_autoload_register(function($classname){
 
 	$classname = explode("\\", $classname);
 	$classname = end($classname);
-	require $filename = "../app/models/".ucfirst($classname).".php";
+	
+	// Define the paths to core and model files
+	$core_path = "../app/core/{$classname}.php";
+	$controller_path = "../app/controllers/{$classname}.php";
+	$model_path = "../app/models/{$classname}.php";
+
+	// Include the class file if it exists
+    if (file_exists($core_path)) {
+        require_once $core_path;
+    } elseif (file_exists($controller_path)) {
+        require_once $controller_path;
+    } elseif (file_exists($model_path)) {
+        require_once $model_path;
+    }
+	
 });
 
 require 'config.php';
 require 'Controller.php';
-require 'Database.php';
 require 'functions.php';
+require 'Database.php';
 require 'Model.php';
 require 'Pager.php';
 require 'Request.php';
 require 'Session.php';
 require 'App.php';
+
+
+
+
+
+
+
