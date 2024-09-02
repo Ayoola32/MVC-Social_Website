aef5c1e97079207053fd534ec65ff878e2355554
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-02 12:25:21 +0100

    added syntax query ti create nnew users table if not exist and include the columns

--- app/models/User.php
+++ app/models/User.php
@@ -7,8 +7,7 @@ defined('ROOTPATH') OR exit('Access Denied!');
 /**
  * User class
  */
-class User
-{
+class User{
 	
 	use Model;
 
@@ -16,38 +15,85 @@ class User
 
 	protected $allowedColumns = [
 
-		'email',
+		'user_id',
+		'username',
 		'password',
+		'user_email',
+		'user_firstname',
+		'user_lastname',
+		'user_image',
+		'date'
 	];
 
-	public function validate($data)
-	{
+	public function validate($data){
 		$this->errors = [];
 
-		if(empty($data['email']))
-		{
-			$this->errors['email'] = "Email is required";
-		}else
-		if(!filter_var($data['email'],FILTER_VALIDATE_EMAIL))
-		{
-			$this->errors['email'] = "Email is not valid";
+		if(empty($data['user_email'])){
+			$this->errors['user_email'] = "Email is required";
+		}else if(!filter_var($data['user_email'],FILTER_VALIDATE_EMAIL)){
+			$this->errors['user_email'] = "Email is not valid";
 		}
+
+		if(empty($data['username'])){
+			$this->errors['username'] = "A username is required";
+		}else if(!preg_match("/^[a-zA-Z]+$/", $data['username'])){
+			$this->errors['username'] = "Username can only have letters with no spaces";
+		}
+
 		
-		if(empty($data['password']))
-		{
+		if(empty($data['password'])){
 			$this->errors['password'] = "Password is required";
 		}
+
 		
-		if(empty($data['terms']))
-		{
+		if(empty($data['terms'])){
 			$this->errors['terms'] = "Please accept the terms and conditions";
 		}
 
-		if(empty($this->errors))
-		{
+
+		if(empty($this->errors)){
 			return true;
 		}
 
+
 		return false;
 	}
+
+
+	public function create_table(){
+		$query = "
+			CREATE TABLE IF NOT EXISTS users (
+				user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+				username VARCHAR(50) NOT NULL,
+				password VARCHAR(50) NOT NULL,
+				user_email VARCHAR(50) NOT NULL,
+				user_firstname VARCHAR(50) NOT NULL,
+				user_lastname VARCHAR(50) NOT NULL,
+				user_image TEXT,
+				date DATETIME NULL,
+
+				KEY idx_username (username),
+				KEY idx_email (user_email)
+			);
+		";
+
+		$this->query($query);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
