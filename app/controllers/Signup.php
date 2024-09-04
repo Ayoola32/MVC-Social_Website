ac4888683b5329d8b5570b13f8a136f28b4d54ea
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-04 11:25:51 +0100

    validate user not working, trying to debug where the possible problem is

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -16,14 +16,18 @@ class Signup{
 
 		$req = new Request();
 		if ($req->posted()) {
+
+echo "here1";
 			$user = new User();
 			if ($user->validate($req->post())) {
-
+echo "here2";
 				// Save to database
 				$user->insert($req->post());
 				redirect('login');
+
 			}
 
+show($user->errors);
 			$data['errors'] = $user->errors;
 		}
 
