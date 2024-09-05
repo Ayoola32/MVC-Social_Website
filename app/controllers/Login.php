d53594f150b580b938a4274507fd9300f7b74f45
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-05 11:29:55 +0100

    set up query to handle the login request and redirect if logged in

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -3,6 +3,9 @@
 namespace Controller;
 
 defined('ROOTPATH') OR exit('Access Denied!');
+use \Model\User;
+use \Core\Request;
+use \Core\Session;
 
 /** Login class */
 
@@ -10,8 +13,30 @@ class Login{
 	use MainController;
 
 	public function index(){
+		$data = [];
 
-		$this->view('login');
+		$req = new Request();
+		if ($req->posted()) {
+
+			$user  = new User();
+			$email = $req->post('email');
+			$password = $req->post('password');
+
+			if ($row = $user->first(['email' => $email])){
+				// Check if password is correct
+				if (password_verify($password, $row->password)) {
+					// Authenticate
+					$ses = new Session();
+					$ses->auth($row);
+
+					redirect('home');
+				}
+			}
+			$user->errors['email'] = "Wrong email or password";
+			$data['errors'] = $user->errors;
+			$this->view('login', $data);
+		}
+
 	}
 
 }
