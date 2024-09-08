87bcb9b434182054803cbac47694e5ccb1e2e9c9
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-08 18:33:15 +0100

    redirect unknown page request to 404 only if logged in, and if not logged in redirect to login page

--- app/controllers/_404.php
+++ app/controllers/_404.php
@@ -3,11 +3,18 @@
 namespace Controller;
 
 defined('ROOTPATH') OR exit('Access Denied!');
+use \Core\Session;
+
 
 class _404{
-	use MainController;
+	use MainController, \Model\Database;
 	
 	public function index(){
+		$ses = new Session();
+		if (!$ses->is_logged_in()) {
+			redirect('login');
+		}
+
 		$this->view('404');
 	}
 }
