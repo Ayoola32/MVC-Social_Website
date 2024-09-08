12b4a49e76981d8d8d89a277225fe0425da1e398
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-09 00:02:26 +0100

    make the header and footer reusable and created a folder(include) for the header and footer file in view foldeer

---
+++ app/views/includes/header.view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="<?php echo ROOT?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo ROOT?>/assets/css/style.css" rel="stylesheet">
+
+
+</head>
+<body>
+    
+  <header class="p-3 mb-3 border-bottom shadow">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <i class="h2 mb-1 bi bi-brilliance" ></i>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 link-body-emphasis">Timeline</a></li>
+          <li><a href="#" class="nav-link px-2 link-body-emphasis">Search</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="<?=get_image(user("user_image"))?>" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small">
+            <li><a class="dropdown-item" href="<?=ROOT?>/profile">My Account</a></li>
+            <li><a class="dropdown-item" href="<?=ROOT?>/settings">Edit Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="<?=ROOT?>/logout">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
