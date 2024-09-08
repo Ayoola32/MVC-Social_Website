a4c2e989c370f399aaddb8e64b882f90509fbd19
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-08 18:23:29 +0100

    logout session code implemented to redirect to login page and kill all session

--- app/controllers/Logout.php
+++ app/controllers/Logout.php
@@ -3,15 +3,17 @@
 namespace Controller;
 
 defined('ROOTPATH') OR exit('Access Denied!');
+use \Core\Session;
 
 /** Logout class */
 
 class Logout{
 	use MainController;
-
+	
 	public function index(){
-
-		
+		$ses = new Session();
+		$ses->logout();
+		redirect('login');		
 	}
 
 }
