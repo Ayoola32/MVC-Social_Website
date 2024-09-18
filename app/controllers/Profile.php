13e890f0d0a5a44cddf8110c375baedde5b8b9fe
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-18 09:22:52 +0100

    controller for profile adjusted

--- app/controllers/Profile.php
+++ app/controllers/Profile.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 defined('ROOTPATH') OR exit('Access Denied!');
+use \Model\User;
 use \Core\Session;
 
 /** Profile class */
@@ -11,13 +12,17 @@ class Profile{
 	use MainController;
 
 	public function index(){
+		$user_id = user('user_id');
 		$ses = new Session();
 		if (!$ses->is_logged_in()) {
 			redirect('login');
 		}
 		
+		// get user row
+		$user = new User;
+		$data['row'] = $user->first(['user_id' => $user_id]);
 
-		$this->view('profile');
+		$this->view('profile', $data);
 	}
 
 }
