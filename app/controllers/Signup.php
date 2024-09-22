be80937009d22f8c99b44b537480f2b2b4e7fd11
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2024-09-22 23:14:37 +0100

    implement the userExist method in the controller to confirm if a user already exist

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -18,22 +18,32 @@ class Signup{
 		if ($req->posted()) {
 
 			$user = new User();
-			if ($user->validate($req->post())) {
-				// Save to database
+			$formData = $req->post();
 
-				// Hash Password
-				$randSalt = "alwayskeepchaseYOURgreatness2024tilleternity";
-				$password = password_hash($req->post('password'), PASSWORD_BCRYPT, array($randSalt => 12 ));
-				$req->set('password', $password);
-				$req->set('date', date("Y-m-d H:i:s"));
+			// Check if username or email already exists
+			if ($user->userExists($formData['username'], $formData['email'])) {
+				$user->errors['exists'] = "Username or Email already exists";
+			} else {
+				// If validation passes
+				if ($user->validate($formData)) {
+					// Save to the database
 
-				$user->insert($req->post());
-				redirect('login');
+					// Hash Password
+					$randSalt = "alwayskeepchaseYOURgreatness2024tilleternity";
+					$password = password_hash($req->post('password'), PASSWORD_BCRYPT, array($randSalt => 12 ));
+					$req->set('password', $password);
+					$req->set('date', date("Y-m-d H:i:s"));
 
+					$user->insert($req->post());
+					redirect('login');
+				}
 			}
+
+			// Collect errors to show in the view
 			$data['errors'] = $user->errors;
 		}
 
+		// Load signup view with errors (if any)
 		$this->view('signup', $data);
 	}
 
